c57615919fda9319918c9394a1f820e0a9e12e32
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2022-04-29 14:12:53 +0300

    add a ptoject Guess number

---
+++ Guess the number/app.js
@@ -0,0 +1,14 @@
+const secretNumber = Math.ceil(Mayh.random() * 11);
+let tries = 0;
+
+
+function guessNum(number){
+    if(tries < 5){
+        if(number === secretNumber){
+            alert("You are guess this number");
+        } else {
+             alert("Oh, wrong. Try again!")
+        }
+        tries++;    
+    }else alert("You are lose")
+}
