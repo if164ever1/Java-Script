39b84cca89816f6597d820cdd7da40a146d8e32c
if164ever1 <acere5ihor@gmail.com>
2023-05-26 10:28:37 +0200

    promt

---
+++ JavaScript+React Learning/js/confirm_promt/script.js
@@ -0,0 +1,17 @@
+alert("hello");
+
+const result = confirm("Are you here");
+console.log(result);
+
+const answer = prompt("How old are you?", "18"); //always string
+console.log(answer);
+
+
+const answers = [];
+
+answers[0] = prompt("How old are you?", "");
+answers[1] = prompt("What is your name?", "");
+answers[2] = prompt("How are you?", "");
+
+
+document.write(answers);
